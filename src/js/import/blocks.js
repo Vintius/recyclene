640785d08e07c7ea11acf1c6a68b8f3f274734cd
Vintius <46298999+Vintius@users.noreply.github.com>
2020-03-04 10:57:02 +0300

    Extract dist folder

--- src/js/import/blocks.js
+++
@@ -1,6 +0,0 @@
-import "%blocks%/header/header.js";
-import "%blocks%/footer/footer.js";
-import "%blocks%/main/main.js";
-import "%blocks%/menu/menu.js";
-import "%blocks%/whoSell/whoSell.js";
-import "%blocks%/howItWorks/howItWorks.js"
